bbf4be84d504eeefcb7a57090397534ec79a3a02
Braulio Diez Botella <braulio.diez@gmail.com>
2022-08-24 11:16:17 +0200

    color picker completed

---
+++ 01-binding-color-picker/src/App.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import ColorDisplay from "./color-picker/color-display.svelte";
+  import ColorEditor from "./color-picker/color-editor.svelte";
+  let red = 50;
+  let blue = 200;
+  let green = 10;
+
+  const handleValueChanged = (
+    eventInfo: CustomEvent<{ name: string; value: number }>
+  ) => {
+    switch (eventInfo.detail.name) {
+      case "Red":
+        red = eventInfo.detail.value;
+        break;
+      case "Green":
+        green = eventInfo.detail.value;
+        break;
+      case "Blue":
+        blue = eventInfo.detail.value;
+    }
+  };
+</script>
+
+<main class="root">
+  <div class="display">
+    <ColorDisplay {red} {green} {blue} />
+    R: {red}, G: {green}, B: {blue}
+  </div>
+  <ColorEditor {red} {green} {blue} on:valuechange={handleValueChanged} />
+</main>
+
+<style>
+  .root {
+    display: flex;
+    gap: 50px;
+  }
+
+  .display {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    align-items: space-between;
+  }
+</style>
---
+++ 01-binding-color-picker/src/color-picker/color-editor.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import SingleColorEditor from "./single-color-editor.svelte";
+
+  export let red = 128;
+  export let green = 128;
+  export let blue = 128;
+
+</script>
+
+<div class="root">
+  <SingleColorEditor
+    name="Red"
+    value={red}
+    on:valuechange
+  />
+
+  <SingleColorEditor
+    name="Green"
+    value={green}
+    on:valuechange
+  />
+
+  <SingleColorEditor
+    name="Blue"
+    value={blue}
+    on:valuechange
+  />
+</div>
+
+<style>
+  .root {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+</style>
